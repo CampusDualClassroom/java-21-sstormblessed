b8e5546485d12e5fed13d70a3d73aaca06b348ae
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-26 14:34:56 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise21.java
+++ src/main/java/com/campusdual/classroom/Exercise21.java
@@ -7,23 +7,56 @@ import java.util.TreeSet;
 public class Exercise21 {
 
     public static Set<String> createHashSet() {
-
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("ELEMENT KPRBC");
+        hashSet.add("ELEMENT YPBTM");
+        hashSet.add("ELEMENT AADXU");
+        hashSet.add("ELEMENT RXCGJ");
+        hashSet.add("ELEMENT WYMVD");
+        hashSet.add("ELEMENT WFGEJ");
+        hashSet.add("ELEMENT TYGBS");
+        hashSet.add("ELEMENT MAPTK");
+        hashSet.add("ELEMENT GJXVE");
+        hashSet.add("ELEMENT BAFGL");
+        return hashSet;
     }
 
     public static Set<String> createTreeSet() {
-
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.add("ELEMENT KPRBC");
+        treeSet.add("ELEMENT YPBTM");
+        treeSet.add("ELEMENT AADXU");
+        treeSet.add("ELEMENT RXCGJ");
+        treeSet.add("ELEMENT WYMVD");
+        treeSet.add("ELEMENT WFGEJ");
+        treeSet.add("ELEMENT TYGBS");
+        treeSet.add("ELEMENT MAPTK");
+        treeSet.add("ELEMENT GJXVE");
+        treeSet.add("ELEMENT BAFGL");
+        return treeSet;
     }
 
     private static void printSet(Set<String> customSet) {
-
+        for (String element : customSet) {
+            System.out.println(element);
+        }
     }
 
 
     public static boolean addElementToSet(Set<String> set, String elementToAdd) {
-
+        return set.add(elementToAdd);
     }
 
     public static void main(String[] args) {
+        Set<String> hashSet = createHashSet();
+        Set<String> treeSet = createTreeSet();
+        System.out.println("HashSet:");
+        System.out.println(hashSet);
+        System.out.println(hashSet.size());
+        System.out.println("TreeSet:");
+        System.out.println(treeSet);
+        System.out.println(treeSet.size());
+        System.out.println("HashSet contains element? " + hashSet.contains("ELEMENT KPRBC"));
 
     }
 
